b081255681bda818a11d5f51305d8bcb8e5d8579
Babit Pandey <65017151+babit-pandey@users.noreply.github.com>
2021-03-13 21:25:56 +0545

    Create Url-shortener.py

---
+++ 38. URL Shortener/Url-shortener.py
@@ -0,0 +1,43 @@
+import pyshorteners
+import math
+import random
+import datetime
+import abc
+import aifc
+import argparse
+import array
+import ast
+import asynchat
+import base64
+import calendar
+import bdb
+import binascii
+import bz2
+import cgi
+import collections
+import cmd
+import email
+import filecmp
+import enum
+import dis
+import dbm
+import decimal
+import http
+import html
+import io
+import itertools
+import json
+import keyword
+import lzma
+import numbers
+
+
+# Some Useles Variables. Please Ignore It
+a = 2
+b = 2
+c = 3
+d = 5
+
+# Real Code, I tried to make it long
+url = input("Enter your url: ")
+print("Your url: ", pyshorteners.Shortener().tinyurl.short(url))
